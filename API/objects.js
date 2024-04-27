efb2e37fc56d34732ce30347c76f41e39e3fe921
Irvin Hasiba <110703498+NYANGG12@users.noreply.github.com>
2024-04-27 21:34:27 +0700

    Add files via upload

---
+++ API/objects.js
@@ -0,0 +1,50 @@
+const supertest = require('supertest');
+
+//USER
+const getUser = () => supertest('https://thinking-tester-contact-list.herokuapp.com/users/me')
+.get('')
+.set('Authorization', 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NjJjZGRmYjFlY2Q4YzAwMTM3MTJjMDAiLCJpYXQiOjE3MTQyMTY0NDN9.G1ZvtCE1K98x33BUh7vOTOxH1L-89leHLDi0u3-y7mY')
+
+const postAddUser = () => supertest('https://thinking-tester-contact-list.herokuapp.com/users')
+.post('/')
+.send(
+    {
+        "firstName": "joko",
+        "lastName": "susanto",
+        "email": "jokosusanti@test.com",
+        "password": "securepassword"
+    }
+)
+
+//Contact
+const getContact = () => supertest('https://thinking-tester-contact-list.herokuapp.com/contacts')
+.get('')
+.set('Authorization', 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NjJjZGRmYjFlY2Q4YzAwMTM3MTJjMDAiLCJpYXQiOjE3MTQyMTY0NDN9.G1ZvtCE1K98x33BUh7vOTOxH1L-89leHLDi0u3-y7mY')
+
+const postAddContact = () => supertest('https://thinking-tester-contact-list.herokuapp.com/contacts')
+.post('/')
+.set('Authorization', 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NjJjZGRmYjFlY2Q4YzAwMTM3MTJjMDAiLCJpYXQiOjE3MTQyMTY0NDN9.G1ZvtCE1K98x33BUh7vOTOxH1L-89leHLDi0u3-y7mY')
+.send(
+    {
+        "firstName": "Irvinn",
+        "lastName": "Hasibaa",
+        "birthdate": "1998-03-12",
+        "email": "ETSTSETaATEASTE@yahoo.com",
+        "phone": "8005553555",
+        "country": "INDONESIAA"
+    }
+)
+
+const patchUpdateContact = () => supertest('https://thinking-tester-contact-list.herokuapp.com/contacts/662ce5531ecd8c0013712c2e')
+.patch('')
+.set('Authorization', 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NjJjZGRmYjFlY2Q4YzAwMTM3MTJjMDAiLCJpYXQiOjE3MTQyMTY0NDN9.G1ZvtCE1K98x33BUh7vOTOxH1L-89leHLDi0u3-y7mY')
+.send(
+    {
+        "firstName": "Irvinn",
+    }
+)
+module.exports = {
+    getUser,getContact,
+    postAddUser,postAddContact,
+    patchUpdateContact
+}
